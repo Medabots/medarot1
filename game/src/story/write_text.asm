a52d3743734eab29c881d138eb89a0381b84c1e5
VariantXYZ <variantxyz@gmail.com>
2017-08-09 05:01:05 -0700

    Add SetupDialog Routine

--- game/src/story/write_text.asm
+++ game/src/story/write_text.asm
@@ -1,3 +1,35 @@
+SECTION "SetupDialog", ROM0[$1c87]
+  ld [$c5c7], a
+  xor a
+  ld [$c5c8], a
+  call $1ab0
+  xor a
+  ld [$c6c0], a
+  ld [$c6c5], a
+  ld [$c6c6], a
+  ld hl, $1cc6
+  ld b, $0
+  ld a, [$c765]
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld [$c6c1], a
+  ld [$c6c4], a
+  ld hl, $9c00
+  ld bc, $0041
+  ld a, [$c5c7]
+  cp $1
+  jr z, .asm_1cbc ; 0x1cb7 $3
+  ld bc, $0021
+.asm_1cbc
+  add hl, bc
+  ld a, h
+  ld [$c6c2], a
+  ld a, l
+  ld [$c6c3], a
+  ret
+; 0x1cc6
+
 SECTION "PutChar", ROM0[$1cc9]
 PutChar:
 	ld a, [$c6c6]
