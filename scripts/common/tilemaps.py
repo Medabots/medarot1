4589709d89b2536247082598a858de1d5cab4eb0
VariantXYZ <variantxyz@gmail.com>
2019-06-04 22:59:20 -0700

    Generate tilemaps where possible

--- scripts/common/tilemaps.py
+++ scripts/common/tilemaps.py
@@ -1,24 +1,8 @@
-import struct
-
-def read_short(rom):
-    return struct.unpack("<H", rom.read(2))[0]
-
-def read_byte(rom):
-    return struct.unpack("B", rom.read(1))[0]
-
-def dump_tilemap(tilemap_bytes, table):
-    tilemap = []
-    for b in tilemap_bytes:
-        if b in table:
-            tilemap += table[b]
-        else:
-            tilemap += '\\x{:02x}'.format(b)
-    return tilemap
-
-MODE_LITERAL = 0
-MODE_REPEAT = 1
+MODE_LIT = 0
+MODE_REP = 1
 MODE_INC = 2
 MODE_DEC = 3
+
 def decompress_tilemap(original):
     tmap = []
     rom = iter(original)
@@ -28,10 +12,10 @@ def decompress_tilemap(original):
         else:
             command = (b >> 6) & 0b11
             count = b & 0b00111111
-            if command == MODE_LITERAL:
+            if command == MODE_LIT:
                 for i in range(count+1):
                     tmap.append(next(rom))
-            elif command == MODE_REPEAT:
+            elif command == MODE_REP:
                 byte = next(rom)
                 for i in range(count+2):
                     tmap.append(byte)
@@ -49,3 +33,69 @@ def decompress_tilemap(original):
             ret.append(0xfe)
         ret.append(t)
     return ret
+
+def compress_mode_literal(idx, tmap, do_compression = False):
+    return 0
+
+def compress_mode_repeat(idx, tmap, do_compression = False):
+    curbyte = tmap[idx]
+    i = 0
+    for i, byte in zip(range(64), tmap[idx + 1:]):
+        if byte != curbyte:
+            break
+        elif do_compression:
+            tmap[idx + 1 + i] = 0xFFFF
+    return i
+
+def compress_mode_increment(idx, tmap, do_compression = False):
+    i = 0
+    curbyte = tmap[idx]
+    for i, byte in zip(range(64), tmap[idx + 1:]):
+        if byte != (curbyte + 1 + i) & 0xFF:
+            break
+        elif do_compression:
+            tmap[idx + 1 + i] = 0xFFFF
+    return i   
+
+def compress_mode_decrement(idx, tmap, do_compression = False):
+    i = 0
+    curbyte = tmap[idx]
+    for i, byte in zip(range(64), tmap[idx + 1:]):
+        if byte != (curbyte - 1 - i) & 0xFF:
+            break
+        elif do_compression:
+            tmap[idx + 1 + i] = 0xFFFF
+    return i
+
+COMPRESSION_METHODS = {
+    MODE_LIT : compress_mode_literal,
+    MODE_REP : compress_mode_repeat,
+    MODE_INC : compress_mode_increment,
+    MODE_DEC : compress_mode_decrement
+}
+def compress_tmap(tmap):
+    idx = 0
+    while idx < len(tmap):
+        best = max(COMPRESSION_METHODS, key=lambda x : COMPRESSION_METHODS[x](idx, tmap))
+        byte_count = COMPRESSION_METHODS[best](idx, tmap, True)
+        if best != MODE_LIT:
+            tmap[idx + 1] = tmap[idx]
+            tmap[idx] = ((best << 6) & 0b11000000) + ((byte_count-1) & 0b00111111) # +1 is implied during decompress
+        else:
+            tmap[idx] |= 0x0100
+        idx += 1 + byte_count
+    tmap = [b for b in tmap if b != 0xFFFF]
+    idx = 0
+    while idx < len(tmap):
+        if tmap[idx] & 0x0100:
+            tmap.insert(idx, 0xFF) # +1 is implied during decompress
+            j = 1
+            while j < 64 and j + idx < len(tmap) and tmap[idx + j] & 0x0100:
+                tmap[idx + j] &= 0xFF
+                tmap[idx] += 1
+                j += 1
+            tmap[idx] &= 0xFF
+            idx += j
+        else:
+            idx += 1
+    return tmap
---
+++ scripts/txt2tmap.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import os, sys
+from shutil import copyfile
+sys.path.append(os.path.join(os.path.dirname(__file__), 'common'))
+from common import utils, tilemaps
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    char_table = utils.read_table("scripts/res/tilemaps.tbl", reverse=True)
+    
+    prebuilt = "game/tilemaps/{}.tmap".format(os.path.splitext(os.path.basename(input_file))[0])
+    if os.path.isfile(prebuilt):
+        print("\tUsing prebuilt {}".format(prebuilt))
+        copyfile(prebuilt, output_file)
+        os.utime(output_file, None)
+        quit()
+    
+    with open(input_file, 'r', encoding='utf-8-sig') as f:
+        mode = f.readline().strip()
+        tmap = []
+        if mode == '[DIRECT]':
+            text = f.read().replace('\n','').replace('\r\n','')
+            tmap = [0x01] + tilemaps.compress_tmap(utils.txt2bin(text, char_table))
+        elif mode == '[OVERLAY]':
+            text = f.read().replace('\r\n','\n')
+            tmap = [0x00] + utils.txt2bin(text, char_table)
+        else:
+            raise TypeError("  Tilemap type is in an incorrect format")
+
+        tmap.append(0xFF)
+        with open(output_file, 'wb') as of:
+            of.write(bytearray(tmap))
