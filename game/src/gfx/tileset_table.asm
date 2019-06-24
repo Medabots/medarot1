c77372d86b5d45bdb21792e5d1bdb1f3fb1db41f
VariantXYZ <variantxyz@gmail.com>
2019-06-23 22:14:41 -0700

    Properly dump and handle tilesets

---
+++ game/src/gfx/tileset_table.asm
@@ -0,0 +1,167 @@
+SECTION "Tileset Table", ROM0[$10F0]
+TilesetTable:
+  dw TilesetInfo1156
+  dw TilesetInfo115B
+  dw TilesetInfo1160
+  dw TilesetInfo1165
+  dw TilesetInfo1156
+  dw TilesetInfo116A
+  dw TilesetInfo116F
+  dw TilesetInfo1174
+  dw TilesetInfo1179
+  dw TilesetInfo117E
+  dw TilesetInfo1188
+  dw TilesetInfo118D
+  dw TilesetInfo1192
+  dw TilesetInfo1197
+  dw TilesetInfo119C
+  dw TilesetInfo11A1
+  dw TilesetInfo11A6
+  dw TilesetInfo11AB
+  dw TilesetInfo11B0
+  dw TilesetInfo11B5
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11AB
+  dw TilesetInfo11BA
+  dw TilesetInfo11BF
+  dw TilesetInfo11C4
+  dw TilesetInfo11C9
+  dw TilesetInfo11CE
+  dw TilesetInfo11D3
+  dw TilesetInfo11D8
+  dw TilesetInfo11DD
+  dw TilesetInfo11E2
+  dw TilesetInfo11E7
+  dw TilesetInfo11EC
+  dw TilesetInfo11F1
+  dw TilesetInfo11F6
+  dw TilesetInfo11FB
+  dw TilesetInfo1200
+  dw TilesetInfo1205
+  dw TilesetInfo120A
+  dw TilesetInfo120F
+  dw TilesetInfo1183
+SECTION "TilesetInfo $1156", ROM0[$1156]
+TilesetInfo1156:
+  dbww BANK(Tileset1156), Tileset1156, $8800
+SECTION "TilesetInfo $115B", ROM0[$115B]
+TilesetInfo115B:
+  dbww BANK(Tileset115B), Tileset115B, $9500
+SECTION "TilesetInfo $1160", ROM0[$1160]
+TilesetInfo1160:
+  dbww BANK(Tileset1160), Tileset1160, $9000
+SECTION "TilesetInfo $1165", ROM0[$1165]
+TilesetInfo1165:
+  dbww BANK(Tileset1165), Tileset1165, $8800
+SECTION "TilesetInfo $116A", ROM0[$116A]
+TilesetInfo116A:
+  dbww BANK(Tileset116A), Tileset116A, $8800
+SECTION "TilesetInfo $116F", ROM0[$116F]
+TilesetInfo116F:
+  dbww BANK(Tileset116F), Tileset116F, $8000
+SECTION "TilesetInfo $1174", ROM0[$1174]
+TilesetInfo1174:
+  dbww BANK(Tileset1174), Tileset1174, $8800
+SECTION "TilesetInfo $1179", ROM0[$1179]
+TilesetInfo1179:
+  dbww BANK(Tileset1179), Tileset1179, $8000
+SECTION "TilesetInfo $117E", ROM0[$117E]
+TilesetInfo117E:
+  dbww BANK(Tileset117E), Tileset117E, $97C0
+SECTION "TilesetInfo $1183", ROM0[$1183]
+TilesetInfo1183:
+  dbww BANK(Tileset1183), Tileset1183, $8000
+SECTION "TilesetInfo $1188", ROM0[$1188]
+TilesetInfo1188:
+  dbww BANK(Tileset1188), Tileset1188, $8800
+SECTION "TilesetInfo $118D", ROM0[$118D]
+TilesetInfo118D:
+  dbww BANK(Tileset118D), Tileset118D, $8000
+SECTION "TilesetInfo $1192", ROM0[$1192]
+TilesetInfo1192:
+  dbww BANK(Tileset1192), Tileset1192, $9400
+SECTION "TilesetInfo $1197", ROM0[$1197]
+TilesetInfo1197:
+  dbww BANK(Tileset1197), Tileset1197, $9480
+SECTION "TilesetInfo $119C", ROM0[$119C]
+TilesetInfo119C:
+  dbww BANK(Tileset119C), Tileset119C, $9480
+SECTION "TilesetInfo $11A1", ROM0[$11A1]
+TilesetInfo11A1:
+  dbww BANK(Tileset11A1), Tileset11A1, $9480
+SECTION "TilesetInfo $11A6", ROM0[$11A6]
+TilesetInfo11A6:
+  dbww BANK(Tileset11A6), Tileset11A6, $9480
+SECTION "TilesetInfo $11AB", ROM0[$11AB]
+TilesetInfo11AB:
+  dbww BANK(Tileset11AB), Tileset11AB, $9480
+SECTION "TilesetInfo $11B0", ROM0[$11B0]
+TilesetInfo11B0:
+  dbww BANK(Tileset11B0), Tileset11B0, $9000
+SECTION "TilesetInfo $11B5", ROM0[$11B5]
+TilesetInfo11B5:
+  dbww BANK(Tileset11B5), Tileset11B5, $9300
+SECTION "TilesetInfo $11BA", ROM0[$11BA]
+TilesetInfo11BA:
+  dbww BANK(Tileset11BA), Tileset11BA, $9000
+SECTION "TilesetInfo $11BF", ROM0[$11BF]
+TilesetInfo11BF:
+  dbww BANK(Tileset11BF), Tileset11BF, $9000
+SECTION "TilesetInfo $11C4", ROM0[$11C4]
+TilesetInfo11C4:
+  dbww BANK(Tileset11C4), Tileset11C4, $9000
+SECTION "TilesetInfo $11C9", ROM0[$11C9]
+TilesetInfo11C9:
+  dbww BANK(Tileset11C9), Tileset11C9, $9000
+SECTION "TilesetInfo $11CE", ROM0[$11CE]
+TilesetInfo11CE:
+  dbww BANK(Tileset11CE), Tileset11CE, $9000
+SECTION "TilesetInfo $11D3", ROM0[$11D3]
+TilesetInfo11D3:
+  dbww BANK(Tileset11D3), Tileset11D3, $9000
+SECTION "TilesetInfo $11D8", ROM0[$11D8]
+TilesetInfo11D8:
+  dbww BANK(Tileset11D8), Tileset11D8, $9000
+SECTION "TilesetInfo $11DD", ROM0[$11DD]
+TilesetInfo11DD:
+  dbww BANK(Tileset11DD), Tileset11DD, $9000
+SECTION "TilesetInfo $11E2", ROM0[$11E2]
+TilesetInfo11E2:
+  dbww BANK(Tileset11E2), Tileset11E2, $9000
+SECTION "TilesetInfo $11E7", ROM0[$11E7]
+TilesetInfo11E7:
+  dbww BANK(Tileset11E7), Tileset11E7, $9000
+SECTION "TilesetInfo $11EC", ROM0[$11EC]
+TilesetInfo11EC:
+  dbww BANK(Tileset11EC), Tileset11EC, $9000
+SECTION "TilesetInfo $11F1", ROM0[$11F1]
+TilesetInfo11F1:
+  dbww BANK(Tileset11F1), Tileset11F1, $9000
+SECTION "TilesetInfo $11F6", ROM0[$11F6]
+TilesetInfo11F6:
+  dbww BANK(Tileset11F6), Tileset11F6, $9000
+SECTION "TilesetInfo $11FB", ROM0[$11FB]
+TilesetInfo11FB:
+  dbww BANK(Tileset11FB), Tileset11FB, $9000
+SECTION "TilesetInfo $1200", ROM0[$1200]
+TilesetInfo1200:
+  dbww BANK(Tileset1200), Tileset1200, $9000
+SECTION "TilesetInfo $1205", ROM0[$1205]
+TilesetInfo1205:
+  dbww BANK(Tileset1205), Tileset1205, $9000
+SECTION "TilesetInfo $120A", ROM0[$120A]
+TilesetInfo120A:
+  dbww BANK(Tileset120A), Tileset120A, $9000
+SECTION "TilesetInfo $120F", ROM0[$120F]
+TilesetInfo120F:
+  dbww BANK(Tileset120F), Tileset120F, $9000
