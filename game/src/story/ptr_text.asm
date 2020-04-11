de23008df117cb8eb147208d41d9f6a67a6897de
VariantXYZ <variantxyz@gmail.com>
2020-04-11 01:31:26 -0700

    Dump and include Attacks/Medarotters

--- game/src/story/ptr_text.asm
+++ game/src/story/ptr_text.asm
@@ -15,6 +15,14 @@ SECTION "Skill", ROMX[$7fc0], BANK[$2]
 SkillsPtr::
 INCLUDE "build/ptrlists/Skills.asm"
 
+SECTION "Attacks", ROMX[$76d2], BANK[$17]
+AttacksPtr::
+INCLUDE "build/ptrlists/Attacks.asm"
+
+SECTION "Medarotters", ROMX[$64e6], BANK[$17]
+MedarottersPtr::
+INCLUDE "build/ptrlists/Medarotters.asm"
+
 SECTION "Load Part Type", ROMX[$74eb], BANK[$1]
 LoadPartType:
   push hl
