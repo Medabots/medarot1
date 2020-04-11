611ae1a00f2c224fa74a4afcea1e5acefad64cf2
VariantXYZ <variantxyz@gmail.com>
2020-04-11 12:31:43 -0700

    Add shop menu sell load routine

--- game/src/story/shop_screen.asm
+++ game/src/story/shop_screen.asm
@@ -118,3 +118,123 @@ LoadShopPartsMenu:
   call $02ca
   ret
 ; 0xc6a0
+LoadShopPartsMenuSell:
+  ld a, [$c88a]
+  ld c, a
+  call $016b
+  ld [$c883], a
+  call $4815
+  cp $ff
+  jp z, $479f
+  push af
+  ld a, [$c88a]
+  ld c, a
+  ld a, [$c886]
+  ld b, $0
+  call $0294
+  ld hl, cBUF01
+  ld b, $4
+  ld c, $4
+  call $02f4
+  ld b, $d
+  ld c, $4
+  call $01dd
+  push hl
+  pop bc
+  pop af
+  ld h, $0
+  ld l, a
+  call $01ec
+  ld b, $11
+  ld c, $4
+  ld e, $18
+  call $02ca
+  ld a, [$c886]
+  inc a
+  ld [$c886], a
+  call $4815
+  cp $ff
+  jp z, $479f
+  push af
+  ld a, [$c88a]
+  ld c, a
+  ld a, [$c886]
+  ld b, $0
+  call $0294
+  ld hl, cBUF01
+  ld b, $4
+  ld c, $6
+  call $02f4
+  ld b, $d
+  ld c, $6
+  call $01dd
+  push hl
+  pop bc
+  pop af
+  ld h, $0
+  ld l, a
+  call $01ec
+  ld b, $11
+  ld c, $6
+  ld e, $18
+  call $02ca
+  ld a, [$c886]
+  inc a
+  ld [$c886], a
+  call $4815
+  cp $ff
+  jp z, $479f
+  push af
+  ld a, [$c88a]
+  ld c, a
+  ld a, [$c886]
+  ld b, $0
+  call $0294
+  ld hl, cBUF01
+  ld b, $4
+  ld c, $8
+  call $02f4
+  ld b, $d
+  ld c, $8
+  call $01dd
+  push hl
+  pop bc
+  pop af
+  ld h, $0
+  ld l, a
+  call $01ec
+  ld b, $11
+  ld c, $8
+  ld e, $18
+  call $02ca
+  ld a, [$c886]
+  inc a
+  ld [$c886], a
+  call $4815
+  cp $ff
+  jp z, $479f
+  push af
+  ld a, [$c88a]
+  ld c, a
+  ld a, [$c886]
+  ld b, $0
+  call $0294
+  ld hl, cBUF01
+  ld b, $4
+  ld c, $a
+  call $02f4
+  ld b, $d
+  ld c, $a
+  call $01dd
+  push hl
+  pop bc
+  pop af
+  ld h, $0
+  ld l, a
+  call $01ec
+  ld b, $11
+  ld c, $a
+  ld e, $18
+  call $02ca
+  ret
+; 0xc7a0
