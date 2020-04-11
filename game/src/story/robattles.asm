1dc3fbaffb646573f41bc8ef0b61a9b10c503bc6
VariantXYZ <variantxyz@gmail.com>
2020-04-11 15:28:01 -0700

    Add Robattle Initialize routine

--- game/src/story/robattles.asm
+++ game/src/story/robattles.asm
@@ -120,3 +120,160 @@ LoadRobattleNames:
   call PutString
   ret
 ; 0x2faa
+
+SECTION "Robattle Screen - Initialize", ROMX[$520c], BANK[$4]
+RobattleScreenSetup:
+  ld a, [$c753]
+  call $02bb
+  ld a, [$c64e]
+  ld [$c745], a
+  xor a
+  ld [$c65a], a
+  ld hl, $af00
+  ld b, $0
+  ld a, [$c65a]
+  ld c, a
+  ld a, $8
+  call $02b8
+  ld a, $3
+  ld [de], a
+  ld a, $1
+  ld hl, $0000
+  add hl, de
+  ld [hl], a
+  ld a, [$c650]
+  ld hl, $5326
+  ld b, $0
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld hl, $0001
+  add hl, de
+  ld [hl], a
+  ld a, [$c656]
+  ld hl, $000b
+  add hl, de
+  ld [hl], a
+  call $0162
+  ld a, [$c5f0]
+  and $3
+  ld hl, $c650
+  ld b, $0
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld hl, $000d
+  add hl, de
+  ld [hl], a
+  call $0162
+  ld a, [$c5f0]
+  and $3
+  ld hl, $c650
+  ld b, $0
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld hl, $000e
+  add hl, de
+  ld [hl], a
+  call $0162
+  ld a, [$c5f0]
+  and $3
+  ld hl, $c650
+  ld b, $0
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld hl, $000f
+  add hl, de
+  ld [hl], a
+  call $0162
+  ld a, [$c5f0]
+  and $3
+  ld hl, $c650
+  ld b, $0
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld hl, $0010
+  add hl, de
+  ld [hl], a
+  ld a, [$c65a]
+  inc a
+  ld hl, $0011
+  add hl, de
+  ld [hl], a
+  ld a, [$c64e]
+  push af
+  call $539b
+  pop af
+  ld [$c64e], a
+  ld hl, $000d
+  add hl, de
+  ld a, [hl]
+  and $7f
+  ld c, a
+  call $02be
+  push de
+  ld b, $9
+  ld hl, $0002
+  add hl, de
+  ld d, h
+  ld e, l
+  ld hl, cBUF01
+.asm_112cb
+  ld a, [hli]
+  ld [de], a
+  inc de
+  dec b
+  jr nz, .asm_112cb ; 0x112cf $fa
+  pop de
+  ld a, $1
+  ld hl, $00c8
+  add hl, de
+  ld [hl], a
+  ld hl, $0080
+  add hl, de
+  ld a, $1
+  ld [hl], a
+  ld hl, $000b
+  add hl, de
+  ld a, [hl]
+  ld hl, $0081
+  add hl, de
+  ld [hl], a
+  ld a, [$c654]
+  ld b, a
+  ld a, [$c934]
+  add b
+  ld c, a
+  sub $b
+  jr c, .asm_112f9 ; 0x112f5 $2
+  ld c, $a
+.asm_112f9
+  ld a, c
+  ld hl, $0083
+  add hl, de
+  ld [hl], a
+  ld a, [$c655]
+  ld b, a
+  ld a, [$c935]
+  add b
+  ld c, a
+  sub $7
+  jr c, .asm_1130e ; 0x1130a $2
+  ld c, $6
+.asm_1130e
+  ld a, c
+  ld hl, $0084
+  add hl, de
+  ld [hl], a
+  ld a, [$c65a]
+  inc a
+  ld [$c65a], a
+  ld a, [$c64e]
+  dec a
+  ld [$c64e], a
+  jp nz, $521c
+  ret
+; 0x11326
