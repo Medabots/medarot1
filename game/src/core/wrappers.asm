80841bbd2bbf7de8263a637212248985f787e065
VariantXYZ <variantxyz@gmail.com>
2020-05-01 00:18:04 -0700

    Add Medarot Equip Screen wrapper call

--- game/src/core/wrappers.asm
+++ game/src/core/wrappers.asm
@@ -32,3 +32,19 @@ WrapRobattleSetupMedarotSelect:
   ld [$c6e0], a
   rst $18
   ret
+
+SECTION "Wrappers_3", ROM0[$090a]
+WrapLoadMedarotPartSelectSetupPartScreen:
+  push af
+  ld a, [$c6e0]
+  ld [$c6e1], a
+  ld a, $2
+  ld [$2000], a
+  ld [$c6e0], a
+  pop af
+  call LoadMedarotPartSelectSetupPartScreen
+  ld a, [$c6e1]
+  ld [$c6e0], a
+  rst $18
+  ret
+; 0x925
