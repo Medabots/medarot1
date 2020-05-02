3b05e9127e824b148e0636a23a53f1af13fd06da
andwhyisit <realmmaster26@hotmail.com>
2020-05-02 18:28:53 +1000

    Lazy disassembly of a robattle state.

---
+++ game/src/core/state_indexes.asm
@@ -0,0 +1,10 @@
+SECTION "State Machine Indexes", WRAM0[$C5C0]
+CoreStateIndex:: ds 1
+CoreSubStateIndex:: ds 1
+
+SECTION "Increment Substate Index", ROM0[$17C3]
+IncSubStateIndex::
+	ld a, [CoreSubStateIndex]
+	inc a
+	ld [CoreSubStateIndex], a
+	ret
