dbf4ff8efb28a067446e888a7b6586ecc4d71225
VariantXYZ <variantxyz@gmail.com>
2020-05-03 03:47:12 -0700

    Tabs to spaces

--- game/src/core/state_indexes.asm
+++ game/src/core/state_indexes.asm
@@ -4,7 +4,7 @@ CoreSubStateIndex:: ds 1
 
 SECTION "Increment Substate Index", ROM0[$17C3]
 IncSubStateIndex::
-	ld a, [CoreSubStateIndex]
-	inc a
-	ld [CoreSubStateIndex], a
-	ret
+  ld a, [CoreSubStateIndex]
+  inc a
+  ld [CoreSubStateIndex], a
+  ret
