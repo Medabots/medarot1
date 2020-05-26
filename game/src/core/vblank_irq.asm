1b7d0de47b78d427913a2b2f49cbfcd2d3972786
VariantXYZ <variantxyz@gmail.com>
2020-05-25 17:37:05 -0700

    Add LCDC Status interrupt disassembly

--- game/src/core/vblank_irq.asm
+++ game/src/core/vblank_irq.asm
@@ -33,3 +33,104 @@ VBlankingIRQ::
   pop bc
   pop af
   reti
+
+; LCDC Status Interrupt (INT 48)
+LCDC_Status_IRQ: ; 4d0 (0:4d0)
+  push af
+  push bc
+  push de
+  push hl
+  ld a, [$c7f9]
+  or a
+  jp nz, .asm_51f
+  ld a, [$c6bf]
+  or a
+  jp nz, .asm_4e8
+  call .asm_54e
+  jp .asm_549
+.asm_4e8: ; 4e8 (0:4e8)
+  ld de, $c6b0
+.asm_4eb
+  di
+  call WaitLCDController
+  ld a, [de]
+  ei
+  or a
+  jp z, .asm_517
+  ld hl, $1
+  add hl, de
+  ld a, [hl]
+  ld [$ff43], a
+  ld hl, $2
+  add hl, de
+  ld a, [hl]
+  ld [$ff42], a
+  ld hl, $0
+  add hl, de
+  ld a, [hl]
+  ld b, a
+.asm_509
+  ld a, [$ff44]
+  sub b
+  jr c, .asm_509
+  ld hl, $3
+  add hl, de
+  ld d, h
+  ld e, l
+  jp .asm_4eb
+; 0x517
+.asm_517: ; 517 (0:517)
+  xor a
+  ld [$ff43], a
+  ld [$ff42], a
+  jp .asm_549
+.asm_51f: ; 51f (0:51f)
+  xor a
+  ld [$c7fc], a
+  ld de, $90
+  ld hl, $c800
+.asm_529
+  ld a, [hli]
+  ld [$ff43], a
+  ld a, $00
+  ld [$ff42], a
+  ld a, [$c7fc]
+  ld b, a
+.asm_534
+  call WaitLCDController
+  ld a, [$ff44]
+  sub b
+  jr c, .asm_534
+  ld a, [$c7fc]
+  inc a
+  ld [$c7fc], a
+  dec de
+  ld a, d
+  or e
+  jp nz, .asm_529
+.asm_549
+  pop hl
+  pop de
+  pop bc
+  pop af
+  reti
+.asm_54e: ; 54e (0:54e)
+  ld hl, $c6b0
+  xor a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ld [hli], a
+  ret
+; 0x562
