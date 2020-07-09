a05d3cd36ace8ce0ad2caa2e3893d22d63c8ea09
VariantXYZ <variantxyz@gmail.com>
2020-07-08 23:01:28 -0700

    Move version-specific title screen stuff to version folders

--- game/src/titlescreen/core.asm
+++ game/src/titlescreen/core.asm
@@ -13,69 +13,10 @@ TitlescreenStateMachine::
   jp hl
 
 .table
-  dw TitlescreenStateInitialize
+  dw TitlescreenStateInitialize ; Version specific files, check corresponding version folders
   dw $42B6
   dw $42C0
   dw $42C3
   dw $42ED
   dw $4348
   dw $4354
-
-TitlescreenStateInitialize::
-  ld b, 0
-  ld c, 0
-  ld e, 7
-  call JumpLoadTilemap
-  ld a, 6
-  call JumpLoadFont
-  ld a, 7
-  call JumpLoadFont
-  ld a, $B8
-  ld [$C5C2], a
-  ld a, $A
-  ld [$C5C3], a
-  xor a
-  ld [$C7FB], a
-  ld [$C5C4], a
-  ld [$C5C5], a
-  ld a, 4
-  call JumpTable_17d
-  ld de, $C140
-  ld hl, 4
-  add hl, de
-  ld a, $EF
-  ld [hl], a
-  ld a, 1
-  ld hl, 0
-  add hl, de
-  ld [hl], a
-  ld a, $B8
-  ld hl, 2
-  add hl, de
-  ld [hl], a
-  ld a, $60
-  ld hl, 3
-  add hl, de
-  ld [hl], a
-  ld a, 1
-  ld [$C600], a
-  ld a, $4F
-  ld [$C5AA], a
-  ld a, 1
-  ld [$C6BF], a
-  ld hl, $C6B0
-  ld a, $7F
-  ld [hli], a
-  ld a, $70
-  ld [hli], a
-  xor a
-  ld [hli], a
-  ld b, 3
-  ld c, 4
-  ld d, 0
-  ld e, 0
-  ld a, 3
-  call JumpTable_309
-  ld a, 3
-  ldh [$FFA0], a
-  jp JumpIncSubStateIndexWrapper
