f6acbeffd229046e8169010342e7668b5d90c3d9
VariantXYZ <variantxyz@gmail.com>
2020-07-14 01:12:47 -0700

    Add xlsx2list script

---
+++ scripts/xlsx2list.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+## Converts translator-defined XLSX file to CSV file for bin conversion
+## Usage: python3 scripts/xlsx2csv.py ../Medarot\ 1\ Translation\ Sheet.xlsx ./text/dialog
+import sys
+import openpyxl as xl
+from collections import OrderedDict
+import csv
+from os import path
+
+xlsx = sys.argv[1]
+outdir = sys.argv[2]
+
+SHEETS = list(sys.argv[3:])
+
+wb = xl.load_workbook(filename = xlsx)
+
+for sheet in wb.worksheets:
+	if not sheet.title in SHEETS:
+		continue
+	filename = path.join(outdir, "{0}.txt".format(sheet.title))
+	data = sheet.values
+	header = next(data)
+	useful_indices = [i for i,x in enumerate(header) if x not in ['Original', 'Notes', 'OK?', '#'] ]
+
+	with open(filename, 'w') as outfile:
+		for line in data:
+			if line[0] == '#':
+				continue
+			for i in useful_indices:
+				if line[i]:
+					outfile.write(line[i] + "\n")
