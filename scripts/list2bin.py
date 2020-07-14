1692a4b40d83a2b7d7f66be9bee2a208ce88a831
VariantXYZ <variantxyz@gmail.com>
2020-07-14 02:30:08 -0700

    Add a hack to list2bin to deal with models in parts

--- scripts/list2bin.py
+++ scripts/list2bin.py
@@ -17,18 +17,23 @@ if __name__ == '__main__':
 		utils.read_table("scripts/res/tileset_MainSpecial.tbl", reverse=True),
 		utils.read_table("scripts/res/tileset_MenuText.tbl", reverse=True),
 		utils.read_table("scripts/res/dakuten.tbl", reverse=True),
+	])
+
+	char_table2 = utils.merge_dicts([
+		char_table,
 		utils.read_table("scripts/res/tileset_BoldLetters.tbl", reverse=True)
 	])
 
 	with open(output_file, 'wb') as o, open(input_file, 'r', encoding="utf-8") as i:
 		length,term,padbyte = (int(x) if x.isdigit() else literal_eval(x) for x in i.readline().split("|"))
 		char_table['\n'] = term
+		char_table2['\n'] = term
 
 		prefix = "[{}]".format(version_suffix)
 		line = i.readline()
 		while line:
-			for l in length if isinstance(length, tuple) else (length,):
+			for x,l in enumerate(length) if isinstance(length, tuple) else enumerate((length,)):
 				if not line.startswith("[") or line.startswith(prefix):
 					line = line.replace(prefix,"") # Not the best way to do it, but it's good enough
-					o.write(bytearray(utils.txt2bin(line, char_table, pad=l, padbyte=padbyte)))
+					o.write(bytearray(utils.txt2bin(line, char_table if x == 0 else char_table2, pad=l, padbyte=padbyte)))
 				line = i.readline()
