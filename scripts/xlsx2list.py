3ec8497abd9c2db46a500780e6e11a222608b5c1
VariantXYZ <variantxyz@gmail.com>
2020-07-14 18:39:47 -0700

    Apply xlsx2list to pointer lists as well

--- scripts/xlsx2list.py
+++ scripts/xlsx2list.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
-## Converts translator-defined XLSX file to CSV file for bin conversion
-## Usage: python3 scripts/xlsx2csv.py ../Medarot\ 1\ Translation\ Sheet.xlsx ./text/dialog
+## Converts translator-defined XLSX file to text files for bin conversion
+## Usage: python3 scripts/xlsx2list.py ../Medarot\ 1\ Translation\ Sheet.xlsx ./text/lists <List Types>
 import sys
 import openpyxl as xl
 from collections import OrderedDict
@@ -10,7 +10,6 @@ from os import path
 
 xlsx = sys.argv[1]
 outdir = sys.argv[2]
-
 SHEETS = list(sys.argv[3:])
 
 wb = xl.load_workbook(filename = xlsx)
@@ -21,16 +20,18 @@ for sheet in wb.worksheets:
 	filename = path.join(outdir, "{0}.txt".format(sheet.title))
 	data = sheet.values
 	header = next(data)
-	useful_indices = [i for i,x in enumerate(header) if x not in ['Original', 'Notes', 'OK?', '#'] ]
+	useful_indices = [i for i,x in enumerate(header) if x not in ['Original', 'Notes', 'OK?', '#'] and not x.startswith('Prefix') ]
+	prefix_indices = [i for i,x in enumerate(header) if x.startswith('Prefix')]
 
 	with open(filename, 'w') as outfile:
 		print("Writing {0}".format(filename))
 		for line in data:
 			if line[0] == '#':
 				continue
-			for i in useful_indices:
+			outtext = ''
+			for i in prefix_indices: # Prefix for the first element in the row
 				if line[i]:
-					try:
-						outfile.write(line[i] + "\n")
-					except:
-						print("{}: {}".format(sheet.title, line[i]))
+					outtext += line[i]
+			# The remaining text each gets its own line (e.g. for models in part lists)
+			outtext += '\n'.join([line[i] for i in useful_indices if line[i]])
+			print(outtext, file=outfile)
