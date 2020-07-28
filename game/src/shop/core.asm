c4c280700c8060b7c9e96451b28a588c7d27061b
VariantXYZ <variantxyz@gmail.com>
2020-07-27 18:37:55 -0700

    Dump Shop 'buying item' state

--- game/src/shop/core.asm
+++ game/src/shop/core.asm
@@ -34,7 +34,7 @@ ShopMenuStateMachine::
   dw $71BF
   dw $71F0
   dw $724A
-  dw $725B
+  dw ShopBuyPriceDisplayState
   dw $7343
   dw $735A
   dw $7363
@@ -52,7 +52,119 @@ ShopMenuStateMachine::
   dw $75F8
   dw $7608
 
-SECTION "Shop Menu State Machine 2", ROMX[$7446], BANK[$3]
+SECTION "Shop Menu State Machine 2", ROMX[$725b], BANK[$3]
+ShopBuyPriceDisplayState::
+  ld b, 0
+  ld a, [$C890]
+  ld c, a
+  call $429E
+  ld a, 1
+  ld [$C89A], a
+  ld b, $7
+  ld c, $E
+  call $412D
+  call $433A
+  call $5516
+  ld hl, $4947
+  ld a, [$C88A]
+  ld e, a
+  ld d, 0
+  sla e
+  rl d
+  add hl, de
+  ld a, [hli]
+  ld h, [hl]
+  ld l, a
+  ld a, [$C88C]
+  ld e, a
+  ld d, 0
+  add hl, de
+  ld a, [hli]
+  ld e, a
+  ld d, $0
+  ld a, [$c890]
+  ld b, a
+  call $457a
+  ld a, d
+  ld [$c89b], a
+  ld a, e
+  ld [$c89c], a
+  push de
+  pop bc
+  call $429e
+  ld a, $1
+  ld [$c89a], a
+  ld b, $c
+  ld c, $e
+  call $412d
+  call $433a
+  ldh a, [hJPInputChanged]
+  and hJPInputUp
+  jr z, .upNotPressed
+  ld a, [$c887]
+  ld b, a
+  ld a, [$c890]
+  cp b
+  ret nc
+  ld a, $3
+  ld [$ffa1], a
+  ld a, [$c890]
+  inc a
+  ld [$c890], a
+  ret
+.upNotPressed
+  ldh a, [hJPInputChanged]
+  and hJPInputDown
+  jr z, .downNotPressed
+  ld a, [$c890]
+  cp $1
+  ret z
+  ld a, $3
+  ld [$ffa1], a
+  ld a, [$c890]
+  dec a
+  ld [$c890], a
+  ret
+.downNotPressed
+  ldh a, [hJPInputChanged]
+  and hJPInputB
+  jr z, .bNotPressed
+  ld a, $6
+  ld [$ffa1], a
+  ld a, $c
+  ld [$c7f2], a
+  ld a, [$c88b]
+  ld [$c890], a
+  ret
+  jp $451c
+.bNotPressed
+  ldh a, [hJPInputChanged]
+  and hJPInputA
+  ret z
+  ld a, [$c890]
+  ld [$c88d], a
+  call $52cf
+  ld a, [$c93a]
+  cp h
+  jp c, $7337
+  jp nz, $7320
+  ld a, [$c93b]
+  cp l
+  jp c, $7337
+  ld a, $4
+  ld [$ffa1], a
+  ld b, $e
+  ld c, $7
+  ld e, $68
+  call $02ca
+  xor a
+  ld [$c890], a
+  ld a, $15
+  ld [$c7f2], a
+  ret
+; 0xf337
+
+SECTION "Shop Menu State Machine 3", ROMX[$7446], BANK[$3]
 ShopSellPriceDisplayState::
   ld b, 0
   ld a, [$C890]
